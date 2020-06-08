e2c246c4666c8ef963c245287252fb19c7afa8c2
CGH233 <1113713599@qq.com>
2020-06-08 20:52:12 +0800

    更新mail

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[校园论坛]'
     FLASKY_MAIL_SENDER = MAIL_USERNAME
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
